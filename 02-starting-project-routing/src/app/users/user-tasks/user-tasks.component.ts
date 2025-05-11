3cb3e8b7c9fc227226b1505f786f4c45176b08e1
sjtroxel <sjtroxel@alumni.iu.edu>
2025-05-10 19:47:14 -0500

    I'm behind on my videos

---
+++ 02-starting-project-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,35 @@
+import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+})
+export class UserTasksComponent implements OnInit {
+  // userId = input.required<string>();
+  userName = '';
+  private usersService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  // userName = computed(
+  //   () => this.usersService.users.find(u => u.id === this.userId())?.name
+  // );
+
+ngOnInit(): void {
+  console.log(this.activatedRoute);
+  const subscription = this.activatedRoute.paramMap.subscribe({
+    next: paramMap => {
+      this.userName = this.usersService.users.find(
+        (u) => u.id === paramMap.get('userId')
+      )?.name || '';
+    },
+  });
+
+  this.destroyRef.onDestroy(() => subscription.unsubscribe());
+}
+}
